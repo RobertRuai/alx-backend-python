c8dd7164d0a2ea51795be767ca2f140cc584a99e
RobertRuai <robertruai4@gmail.com>
2023-10-28 17:08:35 +0300

    test that a KeyError is raised for the following input

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """test method returns what it is supposed to"""
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test that KeyError is raised for the inputs"""
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(KeyError(str(err.exception)), repr(err.exception))
